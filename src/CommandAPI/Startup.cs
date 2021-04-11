7295ffb79c3545572c7d798b2c7e16dac36762d4
Kevin Brown <kevinbrowncodes@outlook.com>
2021-04-11 14:10:48 -0400

    Added Model and Mock Repository (Jackson 112)

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,11 +14,13 @@ namespace CommandAPI
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // where "services" are registered (Jackson 100)
+        // think of service as both an interface and an implementation of it (Jackson 100)
         public void ConfigureServices(IServiceCollection services)
         {
-            // SECTION 1. Add code below
             services.AddControllers();
+
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,7 +35,6 @@ namespace CommandAPI
 
             app.UseEndpoints(endpoints =>
             {
-                // SECTION 1. Add code below
                 endpoints.MapControllers();
             });
         }
